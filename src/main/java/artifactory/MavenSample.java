a3a0b55cf12932e678c0dc8074c957c4caab836d
Yongfei Hu <yongfei.hu@everbridge.com>
2018-05-08 15:18:38 +0800

    re commit

---
+++ src/main/java/artifactory/MavenSample.java
@@ -0,0 +1,12 @@
+package artifactory;
+
+public class MavenSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This for testing artifactory, github and jenkins");
+
+	}
+
+}
